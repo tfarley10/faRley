029eef4465ffe6fbd909a6b1520cba67fa6a1e30
Ted Farley <tedfarley@Teds-MBP.fios-router.home>
2020-03-04 18:26:05 -0500

    remove uname, pw

--- R/connections.R
+++ R/connections.R
@@ -1,11 +1,14 @@
 #' get local postgres connection
+#'
+#' @param user username for my local db
+#' @param pw password for my local db
 #' @export
-get_swamp <- function(){
+get_swamp <- function(user, pw){
   DBI::dbConnect(RPostgres::Postgres(),
             host = "localhost",
             dbname = "swamp",
-            password = "T832132i",
-            user = "tfarley",
+            password = pw,
+            user = user,
             port = 5432
   )
 }
